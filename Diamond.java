4d5039970cfc69739ad9d66d04be0d283c597d01
kaidenton <notnediak@gmail.com>
2024-09-30 16:19:20 -0400

    added a user input that asks for a number

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int numberInput;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a number");
+    numberInput = scan.nextInt();
+
+    scan.close();
   }
 }
