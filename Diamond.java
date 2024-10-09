b008df9887f62f80a829d43b8d969836e0b8dcd8
kaidenton <notnediak@gmail.com>
2024-10-09 16:37:28 -0400

    added functional printing diamond for odd number inputs and changed integer name

--- Diamond.java
+++ Diamond.java
@@ -2,13 +2,45 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int numberInput;
+    int userInput;
 
-    Scanner scan = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
     System.out.println("Enter a number");
-    numberInput = scan.nextInt();
+    userInput = input.nextInt();
 
-    scan.close();
+
+    int halfOfUserInput = userInput/2;
+
+    //This if/else statement determines if the user input is even or odd
+    if (userInput % 2 == 0) {
+      System.out.println("even");
+    } else {
+      //Top Half of Diamond
+      for(int top = 0; top < halfOfUserInput; top++){
+        for(int space = 0; space < halfOfUserInput - top - 1; space++) {
+          System.out.print(" ");
+        }
+
+        for(int asterisk = 0; asterisk < 2 * top + 1; asterisk++) {
+          System.out.print("*");
+        }
+
+        System.out.println(" ");
+      }
+      //Bottom Half of Diamond
+      for(int bottom = halfOfUserInput - 1; bottom > 0; bottom--) {
+        for(int space = 0; space < halfOfUserInput - bottom; space++) {
+          System.out.print(" ");
+        }
+
+        for(int asterisk = 0; asterisk < 2 * bottom - 1; asterisk++) {
+          System.out.print("*");
+        }
+
+        System.out.println(" ");
+      }
+    }
+    input.close();
   }
 }
