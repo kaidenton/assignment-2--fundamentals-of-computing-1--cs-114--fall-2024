a7f22dd55b250ec7892e54d89e404283fb583d38
kaidenton <notnediak@gmail.com>
2024-10-14 12:31:52 -0400

    added comments to specify if and for statements

--- Diamond.java
+++ Diamond.java
@@ -14,6 +14,7 @@ public class Diamond {
     //This if/else statement determines if the user input is even or odd
     if (userInput % 2 == 0) {
       //Top Half of Diamond
+      //First Two Lines
       for(int top = 0; top < 2; top++) {
         for(int space = 0; space < userInput - top; space++) {
           System.out.print(" ");
@@ -25,7 +26,7 @@ public class Diamond {
 
         System.out.println(" ");
       }
-
+      //After the second line until the middle of the diamond
       for(int top = 2; top < halfOfUserInput + 1; top++) {
         for(int space = 0; space < userInput - 2 * top + 1; space++) {
           System.out.print(" ");
@@ -37,6 +38,7 @@ public class Diamond {
         System.out.println(" ");
       }
       //Bottom Half of Diamond
+      //After the middle of the diamond until the second to last line
       for(int bottom = halfOfUserInput; bottom > 2; bottom--) {
         for(int space = 0; space < userInput - 2 * bottom + 3; space++) {
           System.out.print(" ");
@@ -48,7 +50,7 @@ public class Diamond {
   
         System.out.println(" ");
       }
-      
+      //Last two lines (If user input is 2, it will print one asterisk)
       if(userInput == 2) {
         System.out.print("  *");
         System.out.println(" ");
