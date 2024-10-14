93e62d7d875188ce9df701da1399f14ba0608ffe
kaidenton <notnediak@gmail.com>
2024-10-14 12:02:19 -0400

    added where it will print the two first lines of an even number

--- Diamond.java
+++ Diamond.java
@@ -9,15 +9,26 @@ public class Diamond {
     System.out.println("Enter a number");
     userInput = input.nextInt();
 
-
     int halfOfUserInput = userInput/2;
 
     //This if/else statement determines if the user input is even or odd
     if (userInput % 2 == 0) {
-      System.out.println("even");
-    } else {
       //Top Half of Diamond
-      for(int top = 0; top < halfOfUserInput; top++){
+      for(int top = 0; top < 2; top++) {
+        for(int space = 0; space < userInput - top; space++) {
+          System.out.print(" ");
+        }
+
+        for(int asterisk = 0; asterisk < top + 1; asterisk++) {
+          System.out.print("* ");
+        }
+
+        System.out.println(" ");
+      }
+    } else {
+      //This runs if the user input is an odd number
+      //Top Half of Diamond
+      for(int top = 0; top < halfOfUserInput; top++) {
         for(int space = 0; space < halfOfUserInput - top - 1; space++) {
           System.out.print(" ");
         }
